83e5c64ec12588905a60d5952010d16b4186cacc
frozenopertor <adityagupta11112002@gmail.com>
2024-05-03 14:38:15 +0530

    pages and components created

--- CollegeNotes/src/App.jsx
+++ CollegeNotes/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
-const App = () => {
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Team from "./pages/team";
+import AdminLogin from "./pages/admin-login";
+import Logout from "./pages/logout";
+import Home from "./pages/home";
+import Contact from "./pages/contact";
+import About from "./pages/about";
+export default function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element = {<Home/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
---
+++ CollegeNotes/src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (    
+    <div>
+        <div>Exam-Folder</div>
+        <div>Learn-Folder</div>
+        <div>Team</div>
+        <div>About</div>
+    </div>
+  )
+}
+
+export default Home
